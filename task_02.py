d1ea0b5b95ae91a10f9ba304141bb0c516edd482
ayine Ernest <ayine17@gmail.com>
2015-02-19 04:50:34 +0000

    task_02 raw input and convert the data type to int

---
+++ task_02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""use raw_input() and a type conversion function to ask the user a question and
+store the result as an integer into a new variable named MY_INTEGER
+"""
+
+MY_INTEGER = raw_input('What is the distance from your home to work? ')
+
+MY_INTEGER = int(MY_INTEGER)
